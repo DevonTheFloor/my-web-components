ce51627e7f2e8a4df262b42557a9bde413c720b3
DevonTheFloor <devonthefloor@free.fr>
2022-08-14 17:27:37 +0200

    Add MyHeader sccs

---
+++ myHeader/myHeader.js
@@ -0,0 +1,27 @@
+import { dCrEl } from '../../js/helpers/myDomHelper.js';
+import styles from './my-header.scss';
+import heroIndex from '../../assets/images/guitare-L.jpg'
+
+// Create a class for the element
+export default class MyHeader extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+    // Create a shadow root
+    //const shadow = this.attachShadow({mode: 'open'});
+    const myheader = dCrEl('header');
+    this.appendChild(myheader);
+      //creat <h1></h1>
+      const h1 = dCrEl('h1');
+      myheader.id ="header-id"
+      myheader.appendChild(h1);
+      let h1title= this.getAttribute('h1title');
+      h1.textContent = h1title;      
+      myheader.appendChild(h1);
+      const img = dCrEl('img');
+      img.id ="hero";
+      myheader.appendChild(img);
+    
+    }
+  }
+
